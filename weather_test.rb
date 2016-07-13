22e5426a07a87700e7e3dcf696d422b71d865da2
tony un <tony un>
2016-07-13 06:57:27 -0400

    committing for test block of code.

--- weather_test.rb
+++ weather_test.rb
@@ -10,5 +10,9 @@ class ForecastTest < Minitest::Test
     stub_request(:get,
     "http://api.wunderground.com/api/06d952a02fe95bb3/forecast10day/q/20330.json")
     .to_return(body: response, headers: { content_type: "application/json"})
-
-    f = Forecast.new 
+    q = Forecast.new("06d952a02fe95bb3", 20330)
+    weather_as_hash = q.forecast
+    assert_equal Hash, weather_as_hash.parsed_response.class
+    assert_equal "forecast", weather_as_hash["forecastday"]
+  end
+end
