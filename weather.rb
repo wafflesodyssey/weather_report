b1fc4db405432a5748918c18b39951e969ca252c
tony un <tony un>
2016-07-12 22:47:49 -0400

    finished four points, committing.

--- weather.rb
+++ weather.rb
@@ -8,9 +8,14 @@ require 'webmock/minitest'
 class Weathersummary
   include HTTParty
   base_uri "https://api.wunderground.com/api"
-  def initialize(zipcode, key)
-    @zipcode = zipcode
+  def initialize(key, zipcode)
     @key = key
+    @zipcode = zipcode
+  end
+
+
+  def current_conditions
+    self.class.get("/#{@key}/conditions/q/#{@zipcode}")
   end
 
 
@@ -20,16 +25,16 @@ class Weathersummary
 
 
   def sunrise_sunset
+    self.class.get("/#{@key}/astronomy/q/#{@zipcode}")
   end
 
-  def condition
+  def weather_alerts
+    self.class.get("/#{@key}/alerts/q/#{@zipcode}")
   end
 
-  def hurricanes
-  end
 
 
 
 
-  def perform_request
+
 #http://api.wunderground.com/api/06d952a02fe95bb3/conditions/q/CA/San_Francisco.json
--- weather_test.rb
+++ weather_test.rb
@@ -7,4 +7,8 @@ require "webmock/minitest"
 class ForecastTest < Minitest::Test
   def test_can_get_weather
     response = File.read("./weather_10_day_data.json")
-    
+    stub_request(:get,
+    "http://api.wunderground.com/api/06d952a02fe95bb3/forecast10day/q/20330.json")
+    .to_return(body: response, headers: { content_type: "application/json"})
+
+    f = Forecast.new 
